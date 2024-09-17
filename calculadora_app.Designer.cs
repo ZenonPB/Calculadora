754e6b9a0b5dbcfc0a9192b1422a1c1c7ecc49f1
zenon bergamo <zenon.bergamo321@gmail.com>
2024-09-17 11:25:30 -0300

    ADD: começo do design da calculadora

---
+++ calculadora_app.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace calculadora
+{
+    partial class calculadora_app
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // calculadora_app
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(393, 560);
+            Name = "calculadora_app";
+            Text = "calculadora_app";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
